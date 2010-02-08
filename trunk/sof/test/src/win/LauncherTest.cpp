d6f028f4a3c6fe396e5cd0fcd38f960c2a6cf895
magr74 <magr74@f58fac1e-6b4a-4ba5-a485-32bddc465fbe>
2010-02-08 04:49:12 +0000

    Documentation

--- trunk/sof/test/src/win/LauncherTest.cpp
+++ trunk/sof/test/src/win/LauncherTest.cpp
@@ -25,17 +25,49 @@ using namespace sof::config;
 using namespace sof::framework;
 using namespace sof::instantiation::win;
 
+/**
+ * Helper class for verifying test results.
+ */
 class TestHelper
 {
 	public:
+
+		/**
+		 * Checks if the specified service is registered by the given bundle.
+		 */
 		static int isServiceRegisteredByBundle( IRegistry& reg, const string& bundleName, const string& serviceName, int propsSize );
+		
+		/**  
+		 * Checks if the specified service is used by the given bundle.
+		 */
 		static int isServiceUsedByBundle( IRegistry& reg, const string& bundleName, const string& serviceName, int propsSize );
+
+		/**
+		 * Checks if the specified service listener is registered by the given bundle.
+		 */
 		static int isServiceListenerRegisteredByBundle( IRegistry& reg, const string& bundleName, const string& serviceName );
+		
+		/**
+		 * Checks if a service is registered by the given bundle.
+		 */
 		static int isServiceRegisteredByBundle( IRegistry& reg, const string& bundleName );
+		
+		/**
+		 * Checks if a service is used by the given bundle.
+		 */
 		static int isServiceUsedByBundle( IRegistry& reg, const string& bundleName );
+		
+		/**
+		 * Checks if a service listener is registered by the given bundle.
+		 */
 		static int isServiceListenerRegisteredByBundle( IRegistry& reg, const string& bundleName );
+
+		/**
+		 * Checks if the specified bundle is started.
+		 */
 		static bool isBundleStarted( IRegistry& reg, const string& bundleName );
 };
+
 
 int TestHelper::isServiceListenerRegisteredByBundle( IRegistry& reg, const string& bundleName, const string& serviceName )
 {
