baec36a37dd73c958a0733fa901017a1f0c1006f
magr74 <magr74@f58fac1e-6b4a-4ba5-a485-32bddc465fbe>
2010-02-16 19:43:21 +0000

    Bugfix: Remote registry not synchronizable ID 2952968

--- trunk/sof/remote/registry/src/registry.cpp
+++ trunk/sof/remote/registry/src/registry.cpp
@@ -1,18 +1,19 @@
 #include <CORBA.h>
 #include <coss/CosNaming.h>
-#include "sof/framework/remote/corba/CORBAHelper.h"
-#include "sof/framework/remote/corba/namingservice/CORBANamingService.h"
-#include "sof/framework/remote/corba/registry/CORBARegistryImpl.h"
-#include "sof/framework/remote/corba/RemoteSOFLauncher.h"
 
 #include <list>
 #include <vector>
 #include <iostream>
 
+#include "sof/framework/remote/corba/CORBAHelper.h"
+#include "sof/framework/remote/corba/registry/CORBARegistryImpl.h"
+#include "sof/util/threading/SingleThreaded.h"
+
 using namespace sof::framework::remote;
 using namespace sof::framework::remote::corba;
 using namespace sof::framework::remote::corba::namingservice;
 using namespace sof::framework::remote::corba::registry;
+using namespace sof::util::threading;
 
 int main( int argc, char **argv)
 {
@@ -24,7 +25,9 @@ int main( int argc, char **argv)
 	}
 
 	CORBAHelper corbaHelper( args );
-	CORBARegistryImpl registry;
+
+	// Note: Use Mutex-Class if you want to synchronize the access to the remote registry!
+	CORBARegistryImpl<SingleThreaded> registry;
 	CORBA::Object_var obj = corbaHelper.activateObject( &registry );
 	corbaHelper.registerObject( obj, CORBAHelper::REMOTE_REGISTRY_PATH, CORBAHelper::REMOTE_REGISTRY_NAME );
 	cout << "Registry service started!" << endl;
