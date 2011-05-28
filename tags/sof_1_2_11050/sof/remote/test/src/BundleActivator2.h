2ed867da67fdef9f26a4b6ff6934cf2c41366bfe
magr74 <magr74@f58fac1e-6b4a-4ba5-a485-32bddc465fbe>
2011-05-28 05:46:43 +0000

    tagging sof_1.2_11050

---
+++ tags/sof_1_2_11050/sof/remote/test/src/BundleActivator2.h
@@ -0,0 +1,61 @@
+#ifndef BUNDLE_ACTIVATOR2_H
+#define BUNDLE_ACTIVATOR2_H
+
+#include "sof/framework/remote/corba/IRemoteBundleActivator.h"
+#include "sof/framework/remote/corba/IRemoteBundleContext.h"
+
+#include "sof/framework/IServiceTrackerCustomizer.h"
+#include "sof/framework/remote/corba/RemoteServiceTracker.h"
+
+#include "Multiplier.h"
+
+using namespace sof::framework::remote::corba;
+using namespace sof::framework;
+
+/**
+ * Implements a test bundle and tracks services.
+ */
+class BundleActivator2 : public IRemoteBundleActivator, public IRemoteServiceTrackerCustomizer
+{
+	private:
+
+		/**
+		 * Tracks the test services of test bundle 1
+		 */
+		RemoteServiceTracker* tracker;
+
+
+	public:
+
+		/** 
+		 * Creates instances of class <code>BundleActivator2</code>.
+		 */
+		BundleActivator2();
+
+		/**
+		 * Destroys the test bundle.
+		 */
+		virtual ~BundleActivator2();
+
+		/**
+		 * Starts the test bundle.
+		 */
+		virtual void start( IRemoteBundleContext::ConstPtr context );	
+		
+		/**
+		 * Stops the test bundle.
+		 */
+		virtual void stop( IRemoteBundleContext::ConstPtr context );
+
+		/**
+		 * Listener method is called when services are started.
+		 */
+		virtual bool addingService( const RemoteServiceReference& ref );
+
+		/**
+		 * Listener method is called when services were removed.
+		 */
+		virtual void removedService( const RemoteServiceReference& ref );
+};
+
+#endif
