724968b6a6a1cc3687587a600b6074748fa528c2
magr74 <magr74@f58fac1e-6b4a-4ba5-a485-32bddc465fbe>
2012-05-22 19:43:43 +0000

    Target for generating the C++ files from IDL files added

---
+++ trunk/sof/remote/idl/idl.cmake
@@ -0,0 +1,17 @@
+
+if ( WIN32 )
+    SET ( ENV{PATH} $ENV{MICO_HOME}\\win32-bin\\lib )
+    FIND_PROGRAM( CORBAIDL $ENV{MICO_HOME}/win32-bin/idl )    
+endif ( WIN32 )
+
+if ( UNIX )
+    # TODO
+endif ( UNIX )
+
+EXECUTE_PROCESS(
+    COMMAND ${CORBAIDL} --c++-suffix=cpp --use-quotes CORBAAdminObjects.idl 
+    COMMAND ${CORBAIDL} --c++-suffix=cpp --use-quotes CORBAObjects.idl
+    RESULT_VARIABLE ret_var )
+
+MESSAGE( STATUS "--> Return value of executed process: -> ${ret_var}")
+
