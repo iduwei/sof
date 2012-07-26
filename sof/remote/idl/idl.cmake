8c00582905536fef504747f98a968d5faa3d137c
magr74 <magr74@f58fac1e-6b4a-4ba5-a485-32bddc465fbe>
2012-07-26 21:27:41 +0000

    CMakeLists.txt files reworked for generating C++ code from IDL files via cmake

--- sof/remote/idl/idl.cmake
+++ sof/remote/idl/idl.cmake
@@ -5,7 +5,9 @@ if ( WIN32 )
 endif ( WIN32 )
 
 if ( UNIX )
-    # TODO
+    SET ( ENV{PATH} $ENV{MICO_HOME}/libs )
+    FIND_PROGRAM( CORBAIDL $ENV{MICO_HOME}/idl/idl    )    
+    MESSAGE( STATUS ${CORBAIDL} )
 endif ( UNIX )
 
 EXECUTE_PROCESS(
