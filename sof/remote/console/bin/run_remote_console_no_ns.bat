f7e3ba882abc2ef6231dc39f239b7f9d7fddb070
magr74 <magr74@f58fac1e-6b4a-4ba5-a485-32bddc465fbe>
2012-08-02 20:28:46 +0000

    Command line parameter for accessing the registry corrected.

--- sof/remote/console/bin/run_remote_console_no_ns.bat
+++ sof/remote/console/bin/run_remote_console_no_ns.bat
@@ -1,3 +1,3 @@
 PATH=%PATH%;%MICO_HOME%\win32-bin\lib;%MICO_HOME%\win32-bin
 
-sof_remote_console.exe  -ORBIIOPAddr inet:localhost:5000 %1 %2
+sof_remote_console.exe  -registry_addr:inet:localhost:5000 %1 %2
