7b5c690e29895331596824abefd0b50a87bad49d
magr74 <magr74@f58fac1e-6b4a-4ba5-a485-32bddc465fbe>
2012-08-02 20:28:46 +0000

    Command line parameter for accessing the registry corrected.

--- trunk/sof/remote/console/bin/run_remote_console_no_ns.sh
+++ trunk/sof/remote/console/bin/run_remote_console_no_ns.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MICO_HOME/libs
-./sof_remote_console  -ORBIIOPAddr inet:localhost:5000 $1 $2
+./sof_remote_console  -registry_addr:inet:localhost:5000 $1 $2
+
 
